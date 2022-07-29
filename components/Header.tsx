4784b5946cda22fd03243bb59287c5af80c63dee
Nazmuz Sakib <sakib@Sakibs-MacBook-Air.local>
2022-07-29 12:46:37 +0600

    completed first build

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <img
+            className="w-14 object-contain cursor-pointer"
+            src="https://assets.website-files.com/609582ff552ac2599aa37c44/61f0c57b890ad9354eb26965_5754350.png"
+            alt=""
+          />
+        </Link>
+        <div className="hidden md:inline-flex  items-center space-x-5">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
+            Follow
+          </h3>
+        </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3 className="">Sign In</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600">
+          Get Started
+        </h3>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
